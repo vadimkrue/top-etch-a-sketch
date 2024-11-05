1a9c9574eb5da233cbafbacf79bdb8c411ad541d
vadimkrue <vadimkrue@gmail.com>
2024-11-05 21:20:58 +0500

    Create user ratio selection logic
    It works, but new divs are added on top of the
    older ones.

--- script.js
+++ script.js
@@ -3,13 +3,46 @@ const cellContainer = document.querySelector('#cell-container');
 const cell = document.createElement('div');
 cell.classList.add('cell');
 
-for (let i = 1; i <= 256; i++) {
+let initialRatio = 16;
+let cellContainerWidth = 400;
+let initialCellSize = cellContainerWidth / initialRatio;
+
+for (let i = 1; i <= (initialRatio * initialRatio); i++) {
 	cellContainer.appendChild(cell.cloneNode(true));
-}
+};
 
 const allCells = Array.from(document.querySelectorAll('.cell'));
+
 allCells.forEach((cell) => {
+	cell.setAttribute('style', `width: ${initialCellSize}px; 
+		height: ${initialCellSize}px`);
 	cell.addEventListener('mouseover', () => {
 		cell.classList.add('bg-black');
 	});
 });
+
+const ratioChoiceBtn = document.querySelector('#ratio-btn');
+ratioChoiceBtn.addEventListener('click', () => {
+	let userRatio = prompt('Choose the size of the sides of the playing field', '');
+	userCellSize = cellContainerWidth / userRatio;
+	
+	for (let i = 1; i <= (userRatio * userRatio); i++) {
+		cellContainer.appendChild(cell.cloneNode(true));
+	};
+	
+	const allCells = Array.from(document.querySelectorAll('.cell'));
+
+	allCells.forEach((cell) => {
+		cell.setAttribute('style', `width: ${userCellSize}px; height: ${userCellSize}px`)
+	})
+
+	allCells.forEach((cell) => {
+		cell.setAttribute('style', `width: ${userCellSize}px; 
+			height: ${userCellSize}px`);
+		cell.addEventListener('mouseover', () => {
+			cell.classList.add('bg-black');
+		});
+	});
+});
+
+
