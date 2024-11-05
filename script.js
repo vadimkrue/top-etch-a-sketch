47e37b85b340a90595de6a2dbc3e892f4ed843f7
vadimkrue <vadimkrue@gmail.com>
2024-11-05 10:41:32 +0500

    Create 'change color on hover' functionality

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const cellContainer = document.querySelector('#cell-container')
+const cellContainer = document.querySelector('#cell-container');
 
 const cell = document.createElement('div');
 cell.classList.add('cell');
@@ -6,3 +6,10 @@ cell.classList.add('cell');
 for (let i = 1; i <= 256; i++) {
 	cellContainer.appendChild(cell.cloneNode(true));
 }
+
+const allCells = Array.from(document.querySelectorAll('.cell'));
+allCells.forEach((cell) => {
+	cell.addEventListener('mouseover', () => {
+		cell.classList.add('bg-black');
+	});
+});
